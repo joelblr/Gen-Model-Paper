9ac5cfc285f82fb77f3008ae8af091c55bd5e126
uhacker <joel022003@gmail.com>
2024-06-02 00:50:24 +0530

    file-path fixes

--- brain.py
+++ brain.py
@@ -1,12 +1,10 @@
-import os
-import sys
-import csv
-import random
+import os, sys
+import csv, random
 from datetime import datetime
 
 
 """ partName: str, freq: int, unique-KeyIndex|ValueIndex: int, limit: int = 1"""
-def getPart(fileName, freq, uniKeyIdx, uniValIdx, limit, used_ids, partId) :
+def getPart(fileName, freq, uniKeyIdx, uniValIdx, limit, partId) :
 
     """
     TODO: make unique based on multi-Column
@@ -73,6 +71,22 @@ def getJsonFormat(placeholder_prefix, val_format_type, csv_object) :
     return json_res
 
 
+""" obj: dict(str: str), new_obj: dict(str: str) """
+def updateObject(obj, new_obj) :
+    for key, val in new_obj :
+        obj[key] = val
+
+
+""" obj: dict[str, str] -> None """
+def getOsDetails() :
+    # Windows
+    osc['bs'] = "\\"
+    osc['os'] = 'nt'
+    if os.name == "posix":  # Linux or macOS
+        osc['bs'] = "/"
+        osc['os'] = 'posix'
+
+
 """ relative_path: str -> str """
 def resource_path(relative_path) :
 
@@ -88,21 +102,15 @@ def resource_path(relative_path) :
 def openDoc(relative_path, prefix_dir) :
 
     file_path = resource_path(f'{prefix_dir}/{relative_path}')
-    print(file_path)
+    # print(file_path)
 
     if os.path.exists(file_path) :
-        if os.name == "nt":  # Windows
+        if osc["os"] == "nt" :      # Windows
             os.startfile(file_path)
-        elif os.name == "posix":  # Linux or macOS
+        elif osc["os"] == "posix":  # Linux or macOS
             os.system("xdg-open " + file_path)
     else :
         print("File not found")
-
-
-""" obj: dict(str: str), new_obj: dict(str: str) """
-def updateObject(obj, new_obj) :
-    for key, val in new_obj :
-        obj[key] = val
 
 
 """ doc: DocxTemplate, folder_path: str, prefix_tpl: str -> str """
@@ -112,7 +120,10 @@ def saveFile(doc, folder_path, prefix_tpl) :
     newfile = resource_path(folder_path +"/" + newfile)
 
     if not os.path.exists(folder_path) :
-        os.system("mkdir -p " + folder_path)
+        if osc["os"] == "nt" :      # Windows
+            os.system("mkdir " + folder_path)
+        elif osc["os"] == "posix":  # Linux or macOS
+            os.system("mkdir -p " + folder_path)
     doc.save(newfile)
 
     return newfile
@@ -122,9 +133,49 @@ def saveFile(doc, folder_path, prefix_tpl) :
 def open_file_explorer(folder_path) :
 
     if os.path.exists(folder_path) :
-        if os.name == "nt":  # Windows
+        if osc["os"] == "nt" :      # Windows
             os.system("start explorer " + folder_path)
-        elif os.name == "posix":  # Linux or macOS
+        elif osc["os"] == "posix":  # Linux or macOS
             os.system("xdg-open " + folder_path)
     else :
         print("File not found")
+
+#----- CACHE ---------------------------------------
+# os-config
+osc = {}
+getOsDetails()
+
+
+# contains set of used-IDs for a given Section/Part A->0, B->1... G->6
+used_ids = {i : set() for i in range(7)}
+
+# total Qs in each part
+used_ids["total"] = [0 for i in range(7)]
+
+used_ids["done"] = [False for i in range(7)]
+used_ids["done"][1] = True  #part_B
+
+# # GET as INPUT-GUI
+inputs = {
+    "prefix_tpl" : input("Enter the Folder Name: ")+"-",
+    "year" : "2025*",
+    "standard" : "SENIOR 1",
+    "tpl" : "SS_Template.docx",
+    "saveas" : "",#as a separate dir for the test papers generated per one run
+}
+
+myDirs = {
+    "pwd" : '.',
+    ".csv" : "data-sets",
+    "tpl" : "templates",
+    "mqp" : resource_path("model-paper"+osc['bs']+inputs["prefix_tpl"][:-1]),
+}
+
+parts_map = {
+    0 : (10, 1, 0, 5),
+    2 : (10, 2, 0, 1),
+    3 : (6, 2, 0, 1),
+    4 : (3, 2, 0, 1),
+    5 : (2, 2, 0, 1),
+    6 : (1, 0, 0, 1),
+}
--- main.py
+++ main.py
@@ -7,76 +7,38 @@ from docxtpl import DocxTemplate
 from brain import *
 from random import randint
 
-# GET as INPUT-GUI
-inputs = {
-    "prefix_tpl" : input("Enter the Folder Name: ")+"-",
-    "year" : "2025*",
-    "saveas" : "",#as a separate dir for the test papers generated per one run
-}
-
-## TODO: use path reference used by Python-Simplified
-myDirs = {
-    "pwd" : '.',
-    ".csv" : "data-sets",
-    "tpl" : "templates",
-    "mqp" : resource_path("model-paper/"+inputs["prefix_tpl"][:-1]),
-}
-
-
-# contains set of used-IDs for a given Section/Part A->0, B->1... G->6
-used_ids = {i : set() for i in range(7)}
-# total Qs in each part
-used_ids["total"] = [0 for i in range(7)]
-#part_B
-used_ids["done"] = [False for i in range(7)]
-used_ids["done"][1] = True
 
 pre_idx = 1
 # to create as many unique papers as possible
 while not all(used_ids["done"]) :
 
-    #templates-map
-    tpls = {
-        "SS": "SS_Template.docx",
-    }
-
-    # GET some inputs from GUI <std, whichLessonsToFocus...>
+    # GET some inputs from GUI <standard, whichLessonsToFocus...>
     context = {
         "year" : inputs['year'],
-        "std" : "Senior 1",
+        "standard" : inputs['standard'],
     }
 
-    tpl_name = resource_path(f'{myDirs["tpl"]}/{tpls["SS"]}')
+    tpl_name = resource_path(f'{myDirs["tpl"]}{osc['bs']}{inputs["tpl"]}')
     doc = DocxTemplate(tpl_name)
-
-    parts_map = {
-        0 : (10, 1, 0, 5),
-        # 1 : (),
-        2 : (10, 2, 0, 1),
-        3 : (6, 2, 0, 1),
-        4 : (3, 2, 0, 1),
-        5 : (2, 2, 0, 1),
-        6 : (1, 0, 0, 1),
-    }
 
 
     csv_file, part_qs = [], []
-    csv_name = resource_path(f'{myDirs[".csv"]}/{context["std"]}/Part_A.csv')
-    csv_file = getPart(csv_name, *parts_map[0], used_ids, 0)
+    csv_name = resource_path(f'{myDirs[".csv"]}{osc['bs']}{context["standard"]}{osc['bs']}Part_A.csv')
+    csv_file = getPart(csv_name, *parts_map[0], 0)
     part_qs = getJsonFormat("part_A", "{3}", csv_file)
     updateObject(context, part_qs)
 
     #TODO: PART B
     for i, c in enumerate("CDEF") :
         csv_file, part_qs = [], []
-        csv_name = resource_path(f'{myDirs[".csv"]}/{context["std"]}/Part_{c}.csv')
-        csv_file = getPart(csv_name, *parts_map[i+2], used_ids, i+2)
+        csv_name = resource_path(f'{myDirs[".csv"]}{osc['bs']}{context["standard"]}{osc['bs']}Part_{c}.csv')
+        csv_file = getPart(csv_name, *parts_map[i+2], i+2)
         part_qs = getJsonFormat(f"part_{c}", "{4} ({2} -> {3}) [{1}]", csv_file)
         updateObject(context, part_qs)
 
     csv_file, part_qs = [], []
-    csv_name = resource_path(f'{myDirs[".csv"]}/{context["std"]}/Part_G.csv')
-    csv_file = getPart(csv_name, *parts_map[6], used_ids, 6)
+    csv_name = resource_path(f'{myDirs[".csv"]}{osc['bs']}{context["standard"]}{osc['bs']}Part_G.csv')
+    csv_file = getPart(csv_name, *parts_map[6], 6)
     part_qs = getJsonFormat("part_G", "{2} [{1}]", csv_file)
     updateObject(context, part_qs)
 
