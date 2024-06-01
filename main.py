8a095074dbac3c1baebe2b5bf2087656a0ab931b
uhacker <joel022003@gmail.com>
2024-06-01 11:19:10 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+"""
+TODO: Run a While-Infinite Loop, Cache all used Qs to form N Unique papers
+"""
+
 # from docx import Document
 from docxtpl import DocxTemplate
 from brain import *
@@ -5,7 +10,7 @@ from random import randint
 
 # GET as INPUT-GUI
 inputs = {
-    "prefix_tpl" : "test ",
+    "prefix_tpl" : "model-paper ",
     "year" : "2025*",
     "saveas" : "",#as a separate dir for the test papers generated per one run
 }
@@ -33,10 +38,22 @@ doc = DocxTemplate(f'{myDirs["tpl"]}/{tpls["SS"]}')
 
 csv, part_qs = [], []
 csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_A.csv', 10, 0, 5)
-part_qs = getJsonFormat("part_a", "{2} ({0})", csv)
+part_qs = getJsonFormat("part_a", "{2}", csv)
 updateObject(context, part_qs)
 
-#TODO: PART B, C, D
+#TODO: PART B
+
+csv, part_qs = [], []
+csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_C.csv', 10, 1, 1)
+part_qs = getJsonFormat("part_c", "{3} ({1} -> {2}) [{0}]", csv)
+updateObject(context, part_qs)
+
+
+csv, part_qs = [], []
+csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_D.csv', 6, 1, 1)
+part_qs = getJsonFormat("part_d", "{3} ({1} -> {2}) [{0}]", csv)
+updateObject(context, part_qs)
+
 
 csv, part_qs = [], []
 csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_E.csv', 3, 1, 1)
@@ -47,6 +64,12 @@ updateObject(context, part_qs)
 csv, part_qs = [], []
 csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_F.csv', 2, 1, 1)
 part_qs = getJsonFormat("part_f", "{3} ({1} -> {2}) [{0}]", csv)
+updateObject(context, part_qs)
+
+
+csv, part_qs = [], []
+csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_G.csv', 1, 1, 1)
+part_qs = getJsonFormat("part_g", "{1} [{0}]", csv)
 updateObject(context, part_qs)
 
 
