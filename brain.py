34569076eca35cae4889bf240326663677c81031
uhacker <joel022003@gmail.com>
2024-06-01 17:55:45 +0530

    Mega Change

--- brain.py
+++ brain.py
@@ -1,43 +1,63 @@
+import os
+import sys
+import csv
+import random
+from datetime import datetime
+
+
 """ partName: str, freq: int, unique-KeyIndex|ValueIndex: int, limit: int = 1"""
-def getPart(fileName, freq, uniKeyIdx, uniValIdx, limit=1) :
+def getPart(fileName, freq, uniKeyIdx, uniValIdx, limit, used_ids, partId) :
 
     """
     TODO: make unique based on multi-Column
             like, year + chapter + pgNum
-    """
-
-    import csv
-    import random
 
     #XXX: handle Errors
+    """
+ 
     with open(fileName) as csvf :
         """ (year, chapter, question) """
         lines = [tuple(line) for line in csv.reader(csvf)]
 
-
+    used_ids["total"][partId] = len(lines)-1
     counter = {line[uniKeyIdx] : [] for line in lines}
     res = []
     for i in range(freq) :
 
         flag = True
         while flag :
-            ques = random.choices(lines[1:])[0]
-            key = ques[uniKeyIdx]
 
-            # if Chapter Undefined or not Repeated
-            if key == "" or (len(counter[key]) < limit and ques[uniValIdx] not in counter[key]) :
+            ques = random.choices(lines[1:])[0]
+            key1 = ques[uniKeyIdx]
+            key2 = ques[uniValIdx]
+
+            if (len(counter[key1]) >= limit) :
+                counter[key1] = []
+
+            if (len(used_ids[partId]) == used_ids["total"][partId]) :
+                used_ids[partId] = set()
+                used_ids["done"][partId] = True
+
+            if (key2 not in used_ids[partId]) :
                 res.append(ques)
-                counter[key].append(ques[uniValIdx])
+                used_ids[partId].add(key2)
+                counter[key1].append(key2)
                 flag = False
 
     # for i, t in enumerate(res) :
     #     print(i, t)
 
+    if (len(used_ids[partId]) == used_ids["total"][partId]) :
+        used_ids[partId] = set()
+        used_ids["done"][partId] = True
+
     # List[tuple(str, str, str), ]
     return res
 
 
-""" placeholder_prefix: str, val_format_type: str, csv_object: List[List[str, str]] """
+""" placeholder_prefix: str, val_format_type: str, csv_object: List[List[str, str]]
+    -> List[List[str, str]]
+"""
 def getJsonFormat(placeholder_prefix, val_format_type, csv_object) :
     json_res = []
 
@@ -53,9 +73,9 @@ def getJsonFormat(placeholder_prefix, val_format_type, csv_object) :
     return json_res
 
 
-""" relative_path: str """
+""" relative_path: str -> str """
 def resource_path(relative_path) :
-    import os, sys
+
     try :
         base_path = sys._MEIPASS
     except Exception :
@@ -67,7 +87,6 @@ def resource_path(relative_path) :
 """ relative_path: str, prefix_dir: str """
 def openDoc(relative_path, prefix_dir) :
 
-    import os
     file_path = resource_path(f'{prefix_dir}/{relative_path}')
     print(file_path)
 
@@ -86,20 +105,26 @@ def updateObject(obj, new_obj) :
         obj[key] = val
 
 
-""" doc: DocxTemplate, prefix_dir: str, dirName: str -> str """
-def saveFile(doc, prefix_dir, prefix_tpl) :
-    from datetime import datetime
+""" doc: DocxTemplate, folder_path: str, prefix_tpl: str -> str """
+def saveFile(doc, folder_path, prefix_tpl) :
 
-    newfile_name = prefix_tpl + datetime.now().strftime("%S%M")
-    newfile_extn = ".docx"
+    newfile = prefix_tpl + datetime.now().strftime("%S%M") + ".docx"
+    newfile = resource_path(folder_path +"/" + newfile)
 
-    newfile = newfile_name + newfile_extn
-    try :
-        doc.save(f'{prefix_dir}/{newfile}')
-    except :
-        import os
-        os.system("mkdir " + prefix_dir)
-        doc.save(f'{prefix_dir}/{newfile}')
+    if not os.path.exists(folder_path) :
+        os.system("mkdir -p " + folder_path)
+    doc.save(newfile)
 
     return newfile
 
+
+""" folder_path: str -> None """
+def open_file_explorer(folder_path) :
+
+    if os.path.exists(folder_path) :
+        if os.name == "nt":  # Windows
+            os.system("start explorer " + folder_path)
+        elif os.name == "posix":  # Linux or macOS
+            os.system("xdg-open " + folder_path)
+    else :
+        print("File not found")
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 """
 TODO: Run a While-Infinite Loop, Cache all used Qs to form N Unique papers
 """
@@ -10,7 +9,7 @@ from random import randint
 
 # GET as INPUT-GUI
 inputs = {
-    "prefix_tpl" : "model-paper",
+    "prefix_tpl" : input("Enter the Folder Name: ")+"-",
     "year" : "2025*",
     "saveas" : "",#as a separate dir for the test papers generated per one run
 }
@@ -18,88 +17,78 @@ inputs = {
 ## TODO: use path reference used by Python-Simplified
 myDirs = {
     "pwd" : '.',
-    "csv" : "data-sets",
+    ".csv" : "data-sets",
     "tpl" : "templates",
-    "mqp" : "model-paper",
+    "mqp" : resource_path("model-paper/"+inputs["prefix_tpl"][:-1]),
 }
 
-#templates-map
-tpls = {
-    "SS": "SS_Template.docx",
-}
 
-# GET some inputs from GUI <std, whichLessonsToFocus...>
-context = {
-    "year" : inputs['year'],
-    "std" : "Senior 1",
-}
+# contains set of used-IDs for a given Section/Part A->0, B->1... G->6
+used_ids = {i : set() for i in range(7)}
+# total Qs in each part
+used_ids["total"] = [0 for i in range(7)]
+#part_B
+used_ids["done"] = [False for i in range(7)]
+used_ids["done"][1] = True
 
-tpl_name = resource_path(f'{myDirs["tpl"]}/{tpls["SS"]}')
-# doc = DocxTemplate(f'{myDirs["tpl"]}/{tpls["SS"]}')
-doc = DocxTemplate(tpl_name)
+pre_idx = 1
+# to create as many unique papers as possible
+while not all(used_ids["done"]) :
 
-csv, part_qs = [], []
-csv_name = resource_path(f'{myDirs["csv"]}/{context["std"]}/Part_A.csv')
-csv = getPart(csv_name, 10, 0, 2, 5)
-part_qs = getJsonFormat("part_a", "{2}", csv)
-updateObject(context, part_qs)
+    #templates-map
+    tpls = {
+        "SS": "SS_Template.docx",
+    }
 
-#TODO: PART B
+    # GET some inputs from GUI <std, whichLessonsToFocus...>
+    context = {
+        "year" : inputs['year'],
+        "std" : "Senior 1",
+    }
 
-csv, part_qs = [], []
-csv_name = resource_path(f'{myDirs["csv"]}/{context["std"]}/Part_C.csv')
-csv = getPart(csv_name, 10, 1, 2, 1)
-part_qs = getJsonFormat("part_c", "{3} ({1} -> {2}) [{0}]", csv)
-updateObject(context, part_qs)
-# csv, part_qs = [], []
-# csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_C.csv', 10, 1, 1)
-# part_qs = getJsonFormat("part_c", "{3} ({1} -> {2}) [{0}]", csv)
-# updateObject(context, part_qs)
+    tpl_name = resource_path(f'{myDirs["tpl"]}/{tpls["SS"]}')
+    doc = DocxTemplate(tpl_name)
 
-csv, part_qs = [], []
-csv_name = resource_path(f'{myDirs["csv"]}/{context["std"]}/Part_D.csv')
-csv = getPart(csv_name, 6, 1, 2, 1)
-part_qs = getJsonFormat("part_d", "{3} ({1} -> {2}) [{0}]", csv)
-updateObject(context, part_qs)
-# csv, part_qs = [], []
-# csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_D.csv', 6, 1, 1)
-# part_qs = getJsonFormat("part_d", "{3} ({1} -> {2}) [{0}]", csv)
-# updateObject(context, part_qs)
-
-csv, part_qs = [], []
-csv_name = resource_path(f'{myDirs["csv"]}/{context["std"]}/Part_E.csv')
-csv = getPart(csv_name, 3, 1, 2, 1)
-part_qs = getJsonFormat("part_e", "{3} ({1} -> {2}) [{0}]", csv)
-updateObject(context, part_qs)
-# csv, part_qs = [], []
-# csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_E.csv', 3, 1, 1)
-# part_qs = getJsonFormat("part_e", "{3} ({1} -> {2}) [{0}]", csv)
-# updateObject(context, part_qs)
-
-csv, part_qs = [], []
-csv_name = resource_path(f'{myDirs["csv"]}/{context["std"]}/Part_F.csv')
-csv = getPart(csv_name, 2, 1, 2, 1)
-part_qs = getJsonFormat("part_f", "{3} ({1} -> {2}) [{0}]", csv)
-updateObject(context, part_qs)
-# csv, part_qs = [], []
-# csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_F.csv', 2, 1, 1)
-# part_qs = getJsonFormat("part_f", "{3} ({1} -> {2}) [{0}]", csv)
-# updateObject(context, part_qs)
-
-csv, part_qs = [], []
-csv_name = resource_path(f'{myDirs["csv"]}/{context["std"]}/Part_G.csv')
-csv = getPart(csv_name, 1, 0, 0, 1)
-part_qs = getJsonFormat("part_g", "{1} [{0}]", csv)
-updateObject(context, part_qs)
-# csv, part_qs = [], []
-# csv = getPart(f'{myDirs["csv"]}/{context["std"]}/Part_G.csv', 1, 1, 1)
-# part_qs = getJsonFormat("part_g", "{1} [{0}]", csv)
-# updateObject(context, part_qs)
+    parts_map = {
+        0 : (10, 1, 0, 5),
+        # 1 : (),
+        2 : (10, 2, 0, 1),
+        3 : (6, 2, 0, 1),
+        4 : (3, 2, 0, 1),
+        5 : (2, 2, 0, 1),
+        6 : (1, 0, 0, 1),
+    }
 
 
-doc.render(context)
+    csv_file, part_qs = [], []
+    csv_name = resource_path(f'{myDirs[".csv"]}/{context["std"]}/Part_A.csv')
+    csv_file = getPart(csv_name, *parts_map[0], used_ids, 0)
+    part_qs = getJsonFormat("part_A", "{3}", csv_file)
+    updateObject(context, part_qs)
 
-newFileName = saveFile(doc, myDirs["mqp"], inputs['prefix_tpl'])
+    #TODO: PART B
+    for i, c in enumerate("CDEF") :
+        csv_file, part_qs = [], []
+        csv_name = resource_path(f'{myDirs[".csv"]}/{context["std"]}/Part_{c}.csv')
+        csv_file = getPart(csv_name, *parts_map[i+2], used_ids, i+2)
+        part_qs = getJsonFormat(f"part_{c}", "{4} ({2} -> {3}) [{1}]", csv_file)
+        updateObject(context, part_qs)
 
-openDoc(newFileName, myDirs["mqp"])
+    csv_file, part_qs = [], []
+    csv_name = resource_path(f'{myDirs[".csv"]}/{context["std"]}/Part_G.csv')
+    csv_file = getPart(csv_name, *parts_map[6], used_ids, 6)
+    part_qs = getJsonFormat("part_G", "{2} [{1}]", csv_file)
+    updateObject(context, part_qs)
 
+
+    doc.render(context)
+
+    newFileName = saveFile(doc, myDirs["mqp"], inputs['prefix_tpl'] + f"{pre_idx}_")
+
+    # openDoc(newFileName, myDirs["mqp"])
+    pre_idx += 1
+
+print("Successfully Downloaded all the Possible Model-Papers")
+print("Check it Out in:")
+print("\t", myDirs["mqp"])
+open_file_explorer(myDirs["mqp"])
